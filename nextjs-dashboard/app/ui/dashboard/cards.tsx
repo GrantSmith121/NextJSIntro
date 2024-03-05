4e304dd3ca6db82a58969bd7c25bb4ff1858ed76
GrrantSmith121 <gss1@iu.edu>
2024-03-05 10:02:48 -0500

    Learning about data fetching

--- nextjs-dashboard/app/ui/dashboard/cards.tsx
+++ nextjs-dashboard/app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,6 +15,13 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
@@ -25,7 +33,7 @@ export default async function CardWrapper() {
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+  /> */}
     </>
   );
 }
